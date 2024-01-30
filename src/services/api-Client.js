73da0881e7c9dabe97f708b604e61bb7f6f4821b
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-30 20:33:04 +0500

    Dabase String

--- src/services/api-Client.js
+++ src/services/api-Client.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:"https://quizdose-backend.vercel.app/api/"
+    baseURL:"https://quizdose-backend-6e67a5546778.herokuapp.com/api/"
 })
