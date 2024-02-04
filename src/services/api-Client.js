f1d4886a92727484d5d3d967ffb0d12ee4d3185d
Muhammad Amamd <muhammadammad146@gmail.com>
2024-02-05 01:47:20 +0500

    update backend url

--- src/services/api-Client.js
+++ src/services/api-Client.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:"https://quizdose-backend-6e67a5546778.herokuapp.com/api/"
+    // baseURL:"https://quizdose-backend-6e67a5546778.herokuapp.com/api/"
+    baseURL:"https://quizdose-backend.vercel.app/api/"
     // baseURL:"http://localhost:5000/api/"
 })
